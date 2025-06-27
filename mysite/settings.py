dd7c3178c4bb3ca4e2981cb80fab74a3fe3f1bb5
BagasN13 <2311102441187@umkt.ac.id>
2025-06-28 04:51:24 +0800

    coba ganti manifest

--- mysite/settings.py
+++ mysite/settings.py
@@ -168,7 +168,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.ManifestStaticFilesStorage'
 
 SITE_ID = 1
 

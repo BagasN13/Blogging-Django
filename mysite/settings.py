e4230acdb7bc8bfff98831d34e6f2c27956ef902
BagasN13 <2311102441187@umkt.ac.id>
2025-06-28 01:27:03 +0800

    Fixed4 Sampai kapan ini

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'https://blogging-django.vercel.app/', # Ganti dengan domain Vercel Anda yang sebenarnya
+    '.vercel.app', # Ini akan mencakup semua subdomain vercel.app
+    'localhost',
+    '127.0.0.1',
+]
 
 # Application definition
 

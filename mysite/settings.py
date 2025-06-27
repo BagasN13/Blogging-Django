e02987eb0a4234665c0a28e45351e6092599c426
BagasN13 <2311102441187@umkt.ac.id>
2025-06-27 17:27:06 +0800

    Update lagi

--- mysite/settings.py
+++ mysite/settings.py
@@ -107,7 +107,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': 'postgresql://postgres:tAerrFpFamJpGhTxjBhlENUSPZlojhSN@postgres.railway.internal:5432/railway'
 }
 
 # Environment Railway

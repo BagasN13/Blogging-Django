8b8615b708c905f6fd4056c28c1da1deca93670d
BagasN13 <2311102441187@umkt.ac.id>
2025-06-28 04:39:27 +0800

    Fixx it

--- mysite/settings.py
+++ mysite/settings.py
@@ -162,8 +162,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static', # ATAU BASE_DIR / 'static' jika itu yang benar
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 

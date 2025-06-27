112da202d42f9b8c83367b244ddbbb3fe97efd3a
BagasN13 <2311102441187@umkt.ac.id>
2025-06-28 03:28:00 +0800

    fixed19

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Pastikan pip terinstal dan terbaru
-python3.12 -m pip install --upgrade pip
+python -m pip install --upgrade pip
 
 # Instal semua dependensi
-python3.12 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # Jalankan collectstatic
-python3.12 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 
 # Jalankan migrasi database
-python3.12 manage.py migrate
+python manage.py migrate

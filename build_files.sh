7c122b0d2844e5eb1776066d361f1a338dab4804
BagasN13 <2311102441187@umkt.ac.id>
2025-06-28 03:36:25 +0800

    coba ini

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Exit on error
-set -o errexit
-
 # Pastikan pip terinstal dan terbaru
-python3 -m pip install --upgrade pip
+python3.12 -m pip install --upgrade pip
 
 # Instal semua dependensi
-python3 -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
 
 # Jalankan collectstatic
-python3 mysite/manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
 
 # Jalankan migrasi database
-python3 mysite/manage.py migrate
+python3.12 manage.py migrate

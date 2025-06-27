53a3be63c6e36c6a5799d7debccda0de24c51558
BagasN13 <2311102441187@umkt.ac.id>
2025-06-28 01:48:08 +0800

    Fixed6 Pak ajarin hosting divercel..

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,6 +1,6 @@
 import os
-import pymysql
-pymysql.install_as_MySQLdb()
+# import pymysql
+# pymysql.install_as_MySQLdb()
 from pathlib import Path
 import dj_database_url
 from decouple import config
